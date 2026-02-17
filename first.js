db95be942fb5021bdbde885dd4b29fc06d90d218
Nasir <connect.nasir.web@gmail.com>
2026-02-17 19:56:48 +0600

    Add HW

--- first.js
+++ first.js
@@ -1,3 +1,56 @@
+let age=Number(prompt("Enter Your Age"));
+let salary=Number(prompt("Enter Your Salary"));
+let live=prompt("Where Do You Live").toUpperCase();
+
+
+
+age>=25 && age<=30?salary>=40000?live==="DHAKA"?console.log("You have passed all Requirment"):console.log("Bad luck"):console.log("Grow up your salary"):console.log("You are not able to marry");
+
+
+
+// // HW
+
+// let user = prompt('Enter a Latter')
+
+// if (user >= 'A' && user <= 'Z') {
+//   console.log('Capital :', user)
+// } else if (user >= 'a' && user <= 'z') {
+//   console.log('Small :', user)
+// } else {
+//   user = prompt('Enter a Number man')
+// }
+
+
+
+
+
+
+// function vowel(str){
+//   let i=0;
+//   for(let char of str){
+//     if(char==="a"||char==="e"||char==="i"||char==="o"||char==="u"){
+//       i++;
+//     }
+//   }
+//   return i;
+// }
+// console.log(vowel("nasiruddin"));
+
+
+
+
+
+
+// let sum= (a=0,b=0)=>{
+//   return a+b;
+// }
+// console.log(sum(3,));
+
+
+
+
+
+
 // let n=Number(prompt("Enter Your First Number"));
 // let m=Number(prompt("Enter Your last Number"));
 // let r=Number(prompt("Enter Your Reject Number"))
@@ -24,26 +77,6 @@
 
 
 
-// function vowel(str){
-//   let i=0;
-//   for(let char of str){
-//     if(char==="a"||char==="e"||char==="i"||char==="o"||char==="u"){
-//       i++;
-//     }
-//   }
-//   return i;
-// }
-// console.log(vowel("nasiruddin"));
-
-
-
-
-// let sum= (a=0,b=0)=>{
-//   return a+b;
-// }
-// console.log(sum(3,));
-
-
 
 
 // Eventlistener
@@ -62,21 +95,3 @@
 //   }
 //   console.log(mode);
 // })
-
-
-
-
-// HW
-
-let user =prompt("Enter a Latter");
-
-
-if(user>= 'A'  && user <='Z' ){
-
-  console.log("Capital :",user);
-
-}else if(user >='a' && user <='z'){
-
-  console.log("Small :",user);
-}
-
